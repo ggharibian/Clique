e8b0c1228062b1e654983257590ac8fe50b87fd4
Anna Tong <annatong@ucla.edu>
2022-02-26 15:53:23 -0800

    Created a sticky navigation bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,18 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./Login";
 import Dashboard from "./Dashboard";
 
+window.onscroll = function() {stickyNav()};
+const navbar = document.getElementById("nav");
+const sticky = navbar.offsetTop;
+
+function stickyNav() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
 function App() {
   return (
     <div className="app">
