920c5b0c2bb2a73da37f7393c66591f7fbd8c4d4
SH <selina.huynh@ymail.com>
2022-03-06 01:00:04 -0800

    Implemented adding other users as friends by email feature
    User can input email in the search bar and add to friends. The database is also updated to reflect this change by appending the friend's uid to the user's friends field.

--- frontend/src/Friends.js
+++ frontend/src/Friends.js
@@ -4,8 +4,11 @@ import "./Friends.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Table from 'react-bootstrap/Table';
 
+import { db } from "./firebase";
+import { getAuth } from "firebase/auth";
+import { query, collection, getDocs, where, doc, updateDoc, arrayUnion } from "firebase/firestore";
 
-function SearchFriend() {
+function searchFriendPopup() {
     const modal = document.getElementById("addfriendpopup");
     const button = document.getElementById("addfriendbtn");
     const span = document.getElementsByClassName("close")[0];
@@ -14,6 +17,64 @@ function SearchFriend() {
     }
     span.onclick = function() {
         modal.style.display = "none";
+    }
+}
+
+const input = document.getElementById("searchFriendInput");
+if (input){
+    const button = document.getElementById("searchfriendbtn");
+    button.onclick = function() {searchFriendEmail()};
+    
+    const searchFriendEmail = async() => {
+        const filter = input.value.toLowerCase();
+        try {
+            const q = query(collection(db, "users"), where("email".toLowerCase(), "==", filter));
+            const querySnapshot = await getDocs(q);
+            try {
+                const friendToAdd = querySnapshot.docs[0].data();
+                const auth = getAuth();
+                const user = auth.currentUser;
+                const currentUserQuery = query(collection(db, "users"), where("uid", "==", user?.uid));
+                const getUserDoc = await getDocs(currentUserQuery);
+                
+                // TODO: (Not urgent) Add better UI for popup boxes instead of alerts and windows confirm
+                if (user.uid == friendToAdd.uid) {
+                    alert("Cannot add yourself as a friend!\nHave you got no friends? :(");
+                }
+                const userData = getUserDoc.docs[0].data();
+                if (userData.friends.includes(friendToAdd.uid)) {
+                    alert(friendToAdd.name + " is already in your friends list.")
+                }
+                else {
+                    // add to table
+                    if (window.confirm("Add " + friendToAdd.name + " as a friend?")) {
+                        const userDocId = getUserDoc.docs[0].id;
+                        addFriendToList(friendToAdd.uid, userDocId);
+                        alert("Successfully added " + friendToAdd.name + " as a friend!");
+                    }
+                }
+            }
+            catch (err) {
+                alert(err.message + "\nUser with email " + input.value + " may not exist");
+            }
+
+          } catch (err) {
+            console.error(err);
+            // alert(err.message);
+          }
+          input.value = "";
+    };
+}
+
+const addFriendToList = async(uid, userDocId) => {
+    try {
+        const userDoc = doc(db, "users", userDocId);
+        await updateDoc(userDoc, {
+            friends: arrayUnion(uid)
+        });
+    } catch (err) {
+        console.error(err);
+        alert("Adding friend unsuccessful.\n" + err.message);
     }
 }
 
@@ -72,11 +133,12 @@ function Friends() {
             <div id="addfriendpopup" class="addfriend">
                 <div class="addfriend-content">
                     <span class="close">&times;</span>
-                    <input type="text" placeholder="Enter your friend's email address"></input>
-                    <p>(Placeholder for 'send friend request' button)</p>
+                    <input type="text" id="searchFriendInput" placeholder="Enter your friend's email address"></input>
+                    {/* TODO: Should be "send friend request button" instead of adding friend right away */}
+                    <button class = "addfriend-sendbutton" id="searchfriendbtn" onclick="searchFriendEmail()">add to friends</button>
                 </div>
             </div>
-            {window.onload = function(){SearchFriend()}}
+            {window.onload = function(){searchFriendPopup()}}
         </div>
         </center>
     );
