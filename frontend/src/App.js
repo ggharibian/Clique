f73f1ca5c4cd21e3cfa92a44b639d73f4ec2a754
Anna Tong <annatong@ucla.edu>
2022-03-06 22:36:37 -0800

    Added a burger for navigation when screen size is shrunk

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,6 +22,31 @@ function stickyNav() {
   }
 }
 
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+
+  // toggle burger navigation bar
+  burger.addEventListener('click', ()=>{
+    nav.classList.toggle('nav-active');
+      // animation of links
+    navLinks.forEach((link, index)=>{
+      if (link.style.animation) {
+        link.style.animation = '';
+      }
+      else{
+        link.style.animation = 'navLinkFade 0.5s ease forwards';
+      }
+    });
+
+    // animate navigation burger
+    burger.classList.toggle('toggle');
+  });
+}
+
+navSlide();
+
 function App() {
   return (
     <div className="app">
