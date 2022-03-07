e52164da5f7574e704d1eb0aabaea096aa014e23
ggharibian <gharibian.garni0@gmail.com>
2022-03-06 19:06:51 -0800

    Update maps script
    Updated the maps script to utilize a custom component that
    was created by another developer online (check the citations fourm
    for citations).

--- frontend/src/Calendar.js
+++ frontend/src/Calendar.js
@@ -3,51 +3,174 @@ import "./Calendar.css";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Carousel from 'react-bootstrap/Carousel';
+import ListGroup from 'react-bootstrap/ListGroup';
+import TimeRange from 'react-time-range';
+
+
+
+function getScheduledMeetings() {
+  return (
+    <div>
+      <div className="cal_regtext"> Upcoming meetings: </div>
+      <Accordion defaultActiveKey="3">
+
+        <Accordion.Item eventKey="3" className="upcoming">
+          <Accordion.Header className="upcoming">Mar 8 - Spring Break Road Trip</Accordion.Header>
+          <Accordion.Body>
+            Details: will be inserted/finalized shortly!
+          </Accordion.Body>
+        </Accordion.Item>
+
+        <Accordion.Item eventKey="4">
+          <Accordion.Header>Mar 16 - One Direction Concert</Accordion.Header>
+          <Accordion.Body>
+            Details: oh I wish this was a real thing...
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+
+      <div className="cal_regtext"> Previous meetings: </div>
+      <Accordion>
+        <Accordion.Item eventKey="2" className="past">
+          <Accordion.Header>Feb 24 - Westwood Dinner</Accordion.Header>
+          <Accordion.Body>
+            The night it all went wrong. The last time we saw him...
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1" className="past">
+          <Accordion.Header>Feb 2 - Cancun Vacation</Accordion.Header>
+          <Accordion.Body>
+            Best trip ever. White sand, beach-front hotel. Ukeleles...
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="0" className="past">
+          <Accordion.Header className="past">Jan 12 - Big Sur Trip</Accordion.Header>
+          <Accordion.Body className="past">
+            This is just filler for now. But Big Sur is amazing omfg.
+          </Accordion.Body>
+        </Accordion.Item>
+
+      </Accordion>
+    </div>
+  );
+}
+
+function ChooseMeetingTImes() {
+  return(
+    <div>
+      <div className="cal_regtext"> Available Days: </div>
+      <Accordion>
+        <Accordion.Item eventKey="3">
+          <Accordion.Header>Mar 4</Accordion.Header>
+          <Accordion.Body>
+            <Row width="10vw">
+              <Col sm={9}>
+                <TimeRange
+                    startMoment="8:00"
+                    endMoment="9:00"
+                    minuteIncrement="15"
+                    onChange={React.returnFunction}
+                />
+              </Col>
+              <Col sm={2}>
+              <Button variant="success">Schedule</Button>{' '}
+              </Col>
+            </Row>
+          </Accordion.Body>
+        </Accordion.Item>
+
+        <Accordion.Item eventKey="4">
+          <Accordion.Header>Mar 8</Accordion.Header>
+          <Accordion.Body>
+          <Row width="10vw">
+            <Col sm={9}>
+              <TimeRange
+                  startMoment="12:00"
+                  endMoment="5:00"
+                  minuteIncrement="15"
+                  onChange={React.returnFunction}
+              />
+            </Col>
+            <Col sm={2}>
+            <Button variant="success">Schedule</Button>{' '}
+            </Col>
+          </Row>
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+
+    </div>
+  );
+}
+
+function getCarousel(){
+  return(
+    <div>
+
+    </div>
+  );
+}
+
+function miscStatistics() {
+  return (
+    <Row className="cal_row" class="h-20" >
+      <Col sm={2}>
+        <div className="cal_bottomtext"> Memories & Metrics: </div>
+      </Col>
+      <Col sm={4}>
+      <ListGroup>
+        <ListGroup.Item>Total Hours Spent: 243</ListGroup.Item>
+        <ListGroup.Item>Average Hrs/Week: 2.64</ListGroup.Item>
+        <ListGroup.Item>Clique Ranking: 1205th</ListGroup.Item>
+      </ListGroup>
+      </Col>
+      <Col sm={2}>
+        <div className="cal_bottomtext"> Surprising Statistics: </div>
+      </Col>
+      <Col sm={4}>
+      <ListGroup>
+        <ListGroup.Item>Always Busy: Garni</ListGroup.Item>
+        <ListGroup.Item>Always Free: Stephanie</ListGroup.Item>
+        <ListGroup.Item>Most Hyped: Selina</ListGroup.Item>
+      </ListGroup>
+      </Col>
+    </Row>
+  );
+}
 
 function Calendar() {
+
     return (
         <div className="calendar">
-        
-        <div className="text">
-            Sample of the table that will be produced
-        </div>
 
-        <Table striped bordered hover>
-            <thead>
-                <tr>
-                <th>Group</th>
-                <th>Date</th>
-                <th>Time</th>
-                <th>Address</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                <td>Group-3</td>
-                <td>February 28</td>
-                <td>2pm</td>
-                <td>UCLA The Study at Hedrick</td>
-                </tr>
-                <tr>
-                <td>Group-2</td>
-                <td>March 2</td>
-                <td>1pm</td>
-                <td>Hammer Museum</td>
-                </tr>
-                <tr>
-                <td>Group-4</td>
-                <td>March 5</td>
-                <td>8am</td>
-                <td>Pauley Pavilion</td>
-                </tr>
-                <tr>
-                <td>Group-1</td>
-                <td>March 13</td>
-                <td>8pm</td>
-                <td>Powell Library</td>
-                </tr>
-            </tbody>
-            </Table>
+          <Row>
+            <Col>
+              <Card border="info" className="cal_widgetCard">
+                  // Todo: Add Calendar widget here!
+              </Card>
+
+              <Card border="info" className="cal_funCard">
+                  {miscStatistics()}
+              </Card>
+            </Col>
+
+            <Col>
+              <Card border="info" className="cal_meetingCard scroll">
+                  {getScheduledMeetings()}
+              </Card>
+
+              <Card border="info" className="cal_scheduleCard">
+                  {ChooseMeetingTImes()}
+              </Card>
+            </Col>
+          </Row>
+
         </div>
     );
 }
--- frontend/src/Routing.js
+++ frontend/src/Routing.js
@@ -4,39 +4,6 @@ import Card from 'react-bootstrap/Card';
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
 import Map from "./components/Map";
 
-// import { Wrapper, Status } from "@googlemaps/react-wrapper";
-
-// import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
-
-// function Map(){
-//     return (
-//         <GoogleMap 
-//             defaultZoom={10} 
-//             defaultCenter = {{lat: 45, lng: -75}}
-//         />
-//     );
-// }
-
-// const WrappedMap = withScriptjs(withGoogleMap(Map));
-
-// class Map extends React.Component{
-//     render(){
-//         return <h1>Map</h1>;
-//     }
-// }
-
-// const Map: React.FC<{}> = () => {
-//     const ref = React.useRef<HTMLDivElement>(null);
-//     const [map, setMap] = React.useState<google.maps.Map>();
-
-//     React.useEffect(() => {
-//     if (ref.current && !map) {
-//         setMap(new window.google.maps.Map(ref.current, {}));
-//     }
-//     }, [ref, map]);
-
-// };
-
 function Routing() {
 
     const center = { lat: -25.363, lng: 131.044 }
@@ -47,9 +14,14 @@ function Routing() {
             <div className="map">
             <Card style={{width: "50%", height: "85%", position: 'absolute', top: '10%', left: '3%'}}>
                 <Card.Body>
-                    <Wrapper apiKey={"AIzaSyBKwEuYU5RNf4DEi4DXyzoPibJwKwyqkkc"}>
-                        <Map center={center} zoom={zoom}/>
-                    </Wrapper>
+                    <Map id="myMap" options={{center: {lat: 0, lng: 0}, zoom: 4}}
+                    onMapLoad = {map => {
+                        // var marker = new window.google.maps.Marker({
+                        //     position: { lat: 0, lng: 0},
+                        //     map: map,
+                        //     title: 'WOWOWOW'
+                        // });
+                    }}/>
                 </Card.Body>
             </Card>
             </div>
